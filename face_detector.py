63b550a35ead0ec6d80c764378bfc267d663bf50
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2022-05-28 17:52:49 -0400

    Refactor face detection implementation into its own class and file to clean up main.py and separate duties

---
+++ face_detector.py
@@ -0,0 +1,55 @@
+import mediapipe as mp
+import cv2
+import pybboxes as pbx
+from google.protobuf.json_format import MessageToDict
+
+class FaceDetector:
+    def __init__(self):
+        self.image = None
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpDrawStyles = mp.solutions.drawing_styles
+
+        self.face = mp.solutions.face_detection.FaceDetection()
+
+        self.cv2_img_coords = []
+        self.face_bounding_box_coords = {}
+
+    def load_image(self, image):
+        self.image = image
+
+    def get_face_data(self):
+        """
+        It takes an image as input, and returns a list of hand landmarks and a list of hand types (right
+        or left)
+        
+        :param image: The image to be processed
+        :return: the hand data and the hand type.
+        """
+
+        # To improve performance, mark the image as not writeable to pass by reference.
+        self.image.flags.writeable = False
+        imageRGB = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        results = self.face.process(imageRGB)
+
+        self.image.flags.writeable = True
+        imageRGB = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            for detection in results.detections:
+                self.face_bounding_box_coords = MessageToDict(detection)['locationData']['relativeBoundingBox']
+
+                H, W, _ = self.image.shape
+                ymin = self.face_bounding_box_coords['ymin']
+                xmin = self.face_bounding_box_coords['xmin']
+                width = self.face_bounding_box_coords['width']
+                height = self.face_bounding_box_coords['height']
+
+                # Covert the YOLO bounding box coordinates to the original image dimensions
+                converted_coords = pbx.convert_bbox((xmin, ymin, width, height), from_type="yolo", to_type="voc", image_width=W, image_height=H)
+                x = converted_coords[0]+42
+                y = converted_coords[1]+42
+                w = converted_coords[2]+42
+                h = converted_coords[3]+42
+                self.cv2_img_coords = [x, y, w, h]
+
+                # Create a bounding box around the face
+                cv2.rectangle(self.image, (x, y), (w, h), (0, 255, 0), 2)
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import cv2
-import mediapipe as mp
-import numpy as np
-import pybboxes as pbx
 import time
-from google.protobuf.json_format import MessageToDict
 
 from hands_detector import HandsDetector
+from face_detector import FaceDetector
 
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0)
-    hands = None
 
     # Choose if you want to detect hands or face
     DETECT_HANDS = True
@@ -17,6 +13,8 @@ if __name__ == "__main__":
 
     if DETECT_HANDS:
         hands = HandsDetector()
+    if DETECT_FACE:
+        face = FaceDetector()
     
     cv2_img_coords = []
     face_bounding_box_coords = {}
@@ -36,36 +34,8 @@ if __name__ == "__main__":
                 hands.update_landmark_positions(hand_data, hand_type)
 
         if DETECT_FACE:
-            with mp.solutions.face_detection.FaceDetection(
-                model_selection=0, min_detection_confidence=0.5) as face_detection:
-
-                # To improve performance, mark the image as not writeable to pass by reference.
-                image.flags.writeable = False
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                results = face_detection.process(image)
-
-                image.flags.writeable = True
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                if results.detections:
-                    for detection in results.detections:
-                        face_bounding_box_coords = MessageToDict(detection)['locationData']['relativeBoundingBox']
-
-                        H, W, _ = image.shape
-                        ymin = face_bounding_box_coords['ymin']
-                        xmin = face_bounding_box_coords['xmin']
-                        width = face_bounding_box_coords['width']
-                        height = face_bounding_box_coords['height']
-
-                        # Covert the YOLO bounding box coordinates to the original image dimensions
-                        converted_coords = pbx.convert_bbox((xmin, ymin, width, height), from_type="yolo", to_type="voc", image_width=W, image_height=H)
-                        x = converted_coords[0]+42
-                        y = converted_coords[1]+42
-                        w = converted_coords[2]+42
-                        h = converted_coords[3]+42
-                        cv2_img_coords = [x, y, w, h]
-
-                        # Create a bounding box around the face
-                        cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)                    
+            face.load_image(image)
+            face.get_face_data()
 
         cv2.imshow("Output", image)
 
